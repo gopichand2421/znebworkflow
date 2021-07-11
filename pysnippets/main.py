206a6eb98959ad35939e6033c650738044eaa04a
gopichand2421 <gopichand2416@gmail.com>
2021-07-11 19:09:45 +0530

    python changes done for building

--- pysnippets/main.py
+++ pysnippets/main.py
@@ -1,5 +1,5 @@
-import os,glob
-
+import os
+import subprocess
 from pathlib import Path
 import shutil
 
@@ -7,11 +7,16 @@ rootDir = str(Path(__file__).resolve().parent.parent)
 
 for file in os.listdir(rootDir):
     if file.endswith(".war"):
-        os.remove(file)
+        filePath = os.path.join(rootDir,file)
+        subprocess.run(f'echo {filePath}',shell=True)
+        print(file+ ' is deleted successfully')
 
 warFolder = str(os.path.join(rootDir,"source\kgcappliction\\target"))
 
 for warfile in os.listdir(warFolder):
     if warfile.endswith(".war"):
         currentfile=str(os.path.join(warFolder,warfile))
-        shutil.copyfile(currentfile,rootDir)
+        print(f'Source directory is: {currentfile}')
+        print(f'Destination directory is {rootDir}')
+        subprocess.call(f'copy {currentfile}  {rootDir}',shell=True)
+        print("file is successfully copied")
